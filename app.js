2d2db6243b001f45db4d035ccdf77a75e4ec2ab2
Sam <stewartsamaira@gmail.com>
2023-07-02 02:59:30 -0400

    Base files for Express biztime

--- app.js
+++ app.js
@@ -1,15 +1,25 @@
-const express = require("express")
-const app = express();
-const itemsRoutes = require("./routes/items")
+/** BizTime express application. */
+
+
+const express = require("express");
+
 const ExpressError = require("./expressError")
+const companiesRoutes = require("./routes/companies");
+const invoicesRoutes = require("./routes/invoices");
+
+const app = express();
+
 
 app.use(express.json());
-app.use("/items", itemsRoutes);
+app.use("/companies", companiesRoutes);
+app.use("/invoices", invoicesRoutes);
+
 
 /** 404 handler */
 
 app.use(function (req, res, next) {
-  return new ExpressError("Not Found", 404);
+  const err = new ExpressError("Not Found", 404);
+  return next(err);
 });
 
 /** general error handler */
@@ -18,8 +28,10 @@ app.use((err, req, res, next) => {
   res.status(err.status || 500);
 
   return res.json({
-    error: err.message,
+    error: err,
+    message: err.message
   });
 });
 
-module.exports = app
+
+module.exports = app;
